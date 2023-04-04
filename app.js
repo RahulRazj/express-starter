dd0521c7c53957bb9d0c8c0f7ccfcfb52d243fc7
Rahul Chaurasiya <rahulch295@gmail.com>
2023-04-04 09:56:40 +0545

    express starter project

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import BootstrapApp from './system/bootstrap.js';
+import RoutesConfig from './routes/index.js';
+
+dotenv.config();
+const app = express();
+
+// Bootstrap Application
+new BootstrapApp(app);
+
+app.use(express.json());
+
+process.on('uncaughtException', (err, req) => {
+	if (err && err.message !== 'TEST') {
+		console.error('whoops! There was an uncaught error', err);
+	}
+});
+
+process.on('unhandledRejection', function (reason, promise, req) {
+	console.error('Unhandled rejection reason', reason);
+	console.error('Unhandled rejection promise', promise);
+});
+
+RoutesConfig(app);
+
+export default app;
